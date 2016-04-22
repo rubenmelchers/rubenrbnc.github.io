96bc41198fdce9e0b9976fe717ea335e53419c2f
rubenrbnc <rubenmelchers1994@gmail.com>
2016-04-23 01:07:19 +0200

    new portfolio
    created a whole new portfolio

---
+++ script/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+
+
+    var options = {
+        $AutoPlay: true
+    };
+    var jssor_slider1 = new $JssorSlider$("slider1_container", options);
+
+});
